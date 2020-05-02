d14d2099d286c327c9789cfcab4d54072cc6dfe7
nshaibu <nafiushaibu1@gmail.com>
2020-05-02 12:40:09 +0000

    update more.js

--- more.js
+++ more.js
@@ -1,17 +1,16 @@
 (function ($) {
 
-    let bottomCaret = `<svg viewBox="0 0 32 32" class="icon icon-caret-bottom" viewBox="0 0 32 32" aria-hidden="true">
-                        <path d="M24 11.305l-7.997 11.39L8 11.305z"/></svg>`;
-    let topCaret = `<svg viewBox="0 0 32 32" class="icon icon-caret-top" viewBox="0 0 32 32" aria-hidden="true">
-                        <path d="M8 20.695l7.997-11.39L24 20.695z"/></svg>`
+    let bottomCaret = `<img src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBkPSJNMjQgMTEuMzA1bC03Ljk5NyAxMS4zOUw4IDExLjMwNXoiLz48L3N2Zz4='/>`;
+    let topCaret = `<img src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBkPSJNOCAyMC42OTVsNy45OTctMTEuMzlMMjQgMjAuNjk1eiIvPjwvc3ZnPg=='/>`;
 
-    function toggle() {}
+    function toggle(content, caret) {}
 
     $('.more-text').each(function () {
         let more_set = $(this).attr('data-more');
         let data_toggle = $(this).attr('data-toggle');
         let maxsize = parseInt($(this).attr('data-maxsize'));
-        maxsize = maxsize? maxsize : 30;
+
+        maxsize = maxsize ? maxsize : 30;
 
         let text = $(this).text().trim();
         let visible_str = text.slice(0, maxsize-1);
@@ -19,7 +18,7 @@
 
         if (more_set !== 'true') {
             if (text.length > maxsize)
-                $(this).html(`${visible_str}<span class="dots">...</span><div class="more">${hidden_str}</div>`);
+                $(this).html(`${visible_str}<span class="dots">...</span><div style="display:none;">${hidden_str}</div>`);
 
             $(this).attr('data-more', true);
 
@@ -39,11 +38,24 @@
                     break;
                 case 'collapse':
                     let content = $(this);
-                    let dots = content.find('.dots');
-                    content.add('.dots').click(function (event) {
-                        console.log($(this));
+                    let truncatedTextCaret = content.find('.dots');
+                    let moreText = content.find('.more');
+
+                    let moreTextCaret = `<span class="more-dots ml-2" style="cursor:pointer">${topCaret}</span>`;
+
+                    truncatedTextCaret.html(bottomCaret);
+                    truncatedTextCaret.addClass('ml-2');
+                    truncatedTextCaret.css("cursor", "pointer");
+
+                    moreText.append(moreTextCaret);
+
+                    truncatedTextCaret.click(function(event) {
+                        console.log($(this))
                     });
-                    content.find('.dots').click(function(event){});
+
+                    moreTextCaret.click(function(event) {
+                        
+                    });
                     break;
             }
         }
